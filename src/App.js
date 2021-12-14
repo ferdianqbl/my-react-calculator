0822ac6607a00c8750aff977100f9642eb197d50
Ferdianqbl <muhammadferdianiqbal@gmail.com>
2021-12-14 22:05:57 +0700

    add react-calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: ''
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  handleClear = () => {
+    this.setState({ number: "" });
+  }
+
+  handleClick = (e) => {
+    this.setState({ number: this.state.number.concat(e.target.name) });
+  }
+
+  handleBackspace = () => {
+    this.setState({ number: this.state.number.slice(0, -1) })
+  }
+
+  handleCalc = () => {
+    try {
+      this.setState({ number: Number(eval(this.state.number).toString()).toFixed(2) });
+
+    } catch (error) {
+      this.setState({ number: 'Invalid Format' });
+    }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1 className="title">React Calculator</h1>
+        <div className="calculator">
+          <input type="text" className="calc-numbers" value={this.state.number} />
+          <div className="calculator-buttons">
+            <button className="btn clear span-2" onClick={this.handleClear}>
+              C
+            </button>
+            <button className="btn orange" onClick={this.handleBackspace}>
+              &larr;
+            </button>
+            <button onClick={this.handleClick} name="/" className="btn orange">
+              &divide;
+            </button>
+            <button onClick={this.handleClick} name="7" className="btn">
+              7
+            </button>
+            <button onClick={this.handleClick} name="8" className="btn">
+              8
+            </button>
+            <button onClick={this.handleClick} name="9" className="btn">
+              9
+            </button>
+            <button onClick={this.handleClick} name="*" className="btn orange">
+              x
+            </button>
+            <button onClick={this.handleClick} name="4" className="btn">
+              4
+            </button>
+            <button onClick={this.handleClick} name="5" className="btn">
+              5
+            </button>
+            <button onClick={this.handleClick} name="6" className="btn">
+              6
+            </button>
+            <button onClick={this.handleClick} name="-" className="btn orange">
+              -
+            </button>
+            <button onClick={this.handleClick} name="1" className="btn">
+              1
+            </button>
+            <button onClick={this.handleClick} name="2" className="btn">
+              2
+            </button>
+            <button onClick={this.handleClick} name="3" className="btn">
+              3
+            </button>
+            <button onClick={this.handleClick} name="+" className="btn orange">
+              +
+            </button>
+            <button onClick={this.handleClick} name="0" className="btn span-3">
+              0
+            </button>
+            <button onClick={this.handleCalc} className="btn orange equal">
+              =
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
